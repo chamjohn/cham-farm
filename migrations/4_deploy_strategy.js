272b96ee590833af99bb983afb313fa723347beb
chamjohn <john@champagne.finance>
2021-02-08 12:02:10 +0800

    deploy sushilp

--- migrations/4_deploy_strategy.js
+++ migrations/4_deploy_strategy.js
@@ -7,7 +7,8 @@ const SushiWethUsdtVault = artifacts.require("Vault")
 const CurveRenbtcWbtcVault = artifacts.require("Vault")
 const StrategyCompoundV1 = artifacts.require("StrategyCompoundV1")
 const StrategyUsdt = artifacts.require("StrategyCompoundV1")
-const StrategySushiWethUsdt = artifacts.require("StrategyCompoundV1")
+const StrategySushi = artifacts.require("StrategySushi")
+const StrategySushiWethUsdt = artifacts.require("StrategySushi")
 
 
 SUB_KEY = "farm"
@@ -17,6 +18,9 @@ USDT = "usdt"
 COMP_CONTROLLER = "unitroller"
 CUSDT = "cerc20_delegator_usdt"
 STRATEGIES = "strategies"
+
+SUSHI_WETH_USDT = "sushi_weth_usdt_pair"
+CERC20_DELEGATOR_SUSHI_WETH_USDT = "cerc20_delegator_sushi_weth_usdt"
 
 module.exports = async function (deployer, network, accounts) {
     console.log("4_deploy_strategy.js, network: ", network)
@@ -50,6 +54,24 @@ module.exports = async function (deployer, network, accounts) {
         }
         // TODO: deploy weth vault
         // TODO: deploy sushi_weth_usdt vault
+        {
+            await deployer.deploy(StrategySushiWethUsdt, {from: accounts[0]});
+            config[network][SUB_KEY][STRATEGIES][SUSHI_WETH_USDT] = StrategySushiWethUsdt.address;
+            let sushiStrategy = await StrategySushi.at(config[network][SUB_KEY][STRATEGIES][SUSHI_WETH_USDT])
+            await sushiStrategy.initialize(
+                accounts[0],
+                accounts[0],
+                config[network][SUB_KEY][CONTROLLER],
+                accounts[0],
+                accounts[0],
+                config[network][COMP_CONTROLLER],
+                config[network][SUSHI_WETH_USDT],
+                {from: accounts[0]}
+            );
+            await controller.approveStrategy(deployedConfig.mainnet[SUSHI_WETH_USDT], config[network][SUB_KEY][STRATEGIES][SUSHI_WETH_USDT])
+            await controller.setVault(deployedConfig.mainnet[SUSHI_WETH_USDT], config[network][SUB_KEY][VAULTS][SUSHI_WETH_USDT])
+            await controller.setStrategy(deployedConfig.mainnet[SUSHI_WETH_USDT], config[network][SUB_KEY][STRATEGIES][SUSHI_WETH_USDT]);
+        }
         // TODO: deploy curve_renbtc_wbtc
         
         
